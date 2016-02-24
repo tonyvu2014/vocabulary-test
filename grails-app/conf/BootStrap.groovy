9d4b2696ea1d1e96b9f7a528e8fe303446f16e19
Tony Vu <tonyvu2008@gmail.com>
2016-02-24 21:40:48 +0800

    Remove duplicate word while loading data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -9,16 +9,23 @@ class BootStrap {
 			def filePath = "resources/word_frequency.txt"
 			def wordFrequencyText = grailsApplication.getParentContext().getResource("classpath:$filePath").inputStream.text
 			print "Loading data..."
+			int r = 1
 			wordFrequencyText.eachLine { line ->
 				try {
 					String[] fields = line.split("\\s+")
 					def wordFrequency = new WordFrequency()
-			    	wordFrequency.rank = fields[0].toInteger()
-					wordFrequency.word = fields[1]
+					def w = fields[1]
+					def record = WordFrequency.findByWord(w)
+					if (record) {
+						return
+					}
+			    	wordFrequency.rank = r
+					wordFrequency.word = w
 					wordFrequency.pos = fields[2]
 					wordFrequency.frequency = fields[3].toInteger()
 					wordFrequency.dispersion = fields[4].toFloat()
 					wordFrequency.save(flush: true, failOnError:true)	
+					r++
 				} catch (Exception ex) {
 					ex.printStackTrace()
 				}
