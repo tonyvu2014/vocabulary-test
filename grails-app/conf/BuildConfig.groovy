4ca6eb14956c9cc77477e5c2270edfd9a0ff8c71
Tony Vu <tonyvu2008@gmail.com>
2018-03-11 23:30:17 +1100

    Update build config

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -49,6 +49,7 @@ grails.project.dependency.resolution = {
 	dependencies {
 		// specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
 		runtime 'mysql:mysql-connector-java:5.1.29'
+		compile 'org.codehaus.groovy.modules.http-builder:http-builder:0.7'
 		// runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
 		test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
 	}
@@ -61,7 +62,8 @@ grails.project.dependency.resolution = {
 		compile ":scaffolding:2.1.2"
 		compile ':cache:1.1.8'
 		compile ":asset-pipeline:1.9.9"
-		
+		compile ':quartz:1.0.2'
+
 		// mail plugin
 		compile "org.grails.plugins:mail:1.0.7"
 		
